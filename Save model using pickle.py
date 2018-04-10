5e81f21ccf1a43e2837360db56fcf5e4c7e11408
ddrightnow <ddrightnow@gmail.com>
2018-04-10 12:50:02 +0800

    er

--- Save model using pickle.py
+++ Save model using pickle.py
@@ -5,6 +5,17 @@ Created on Tue Apr 10 12:34:47 2018
 @author: Dmob
 """
 
+'''
+Finalize Your Model with pickle
+Pickle is the standard way of serializing objects in Python.
+
+You can use the pickle operation to serialize your machine learning algorithms and save the serialized format to a file.
+
+Later you can load this file to deserialize your model and use it to make new predictions.
+
+The example below demonstrates how you can train a logistic regression model on the Pima Indians onset of diabetes dataset, save the model to file and load it to make predictions on the unseen test set
+'''
+
 # Save Model Using Pickle
 import pandas
 from sklearn import model_selection
@@ -32,3 +43,18 @@ pickle.dump(model, open(filename, 'wb'))
 loaded_model = pickle.load(open(filename, 'rb'))
 result = loaded_model.score(X_test, Y_test)
 print(result)
+
+'''
+Running the example saves the model to finalized_model.sav in your local working directory. Load the saved model and evaluating it provides an estimate of accuracy of the model on unseen data.
+'''
+
+
+'''
+Tips for Finalizing Your Model
+This section lists some important considerations when finalizing your machine learning models.
+
+Python Version. Take note of the python version. You almost certainly require the same major (and maybe minor) version of Python used to serialize the model when you later load it and deserialize it.
+Library Versions. The version of all major libraries used in your machine learning project almost certainly need to be the same when deserializing a saved model. This is not limited to the version of NumPy and the version of scikit-learn.
+Manual Serialization. You might like to manually output the parameters of your learned model so that you can use them directly in scikit-learn or another platform in the future. Often the algorithms used by machine learning algorithms to make predictions are a lot simpler than those used to learn the parameters can may be easy to implement in custom code that you have control over.
+Take note of the version so that you can re-create the environment if for some reason you cannot reload your model on another machine or another platform at a later time.
+'''
